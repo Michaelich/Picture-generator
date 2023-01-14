ab526004c37f2e9998e5c919e2883ad11feba122
Michaelich <be.micha.mb@gmail.com>
2023-01-14 20:44:40 +0100

    Added objective function comparing images

---
+++ convertPicture.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+def mse(img1, img2):
+    height, width = img1.shape
+    diff = cv2.subtract(img1, img2)
+    err = np.sum(diff**2)
+    return err/(float(height*width))
+
+def objective_function(img1, img2):
+    im_1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    im_2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    return mse(im_1, im_2)
+
